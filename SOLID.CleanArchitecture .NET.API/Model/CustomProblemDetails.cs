3b7a3690aae2fb6d08c49df3b32b68bd0bc70b6e
Mohammad-Sayyad <sayyadamir71111@gmail.com>
2024-12-13 00:00:54 +0330

    reza changed it again

--- SOLID.CleanArchitecture .NET.API/Model/CustomProblemDetails.cs
+++ SOLID.CleanArchitecture .NET.API/Model/CustomProblemDetails.cs
@@ -4,7 +4,7 @@ namespace SOLID.CleanArchitecture_.NET.API.Model
 {
     public class CustomProblemDetails : ProblemDetails
     {
-        //
+        // reza
         public IDictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
     }
 }
