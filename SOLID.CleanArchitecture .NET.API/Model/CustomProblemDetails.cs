2b4dbb49b85fa071f0135f0a182ebac0812dd0a0
Mohammad-Sayyad <sayyadamir71111@gmail.com>
2024-12-12 23:51:43 +0330

    new order in customproblemdetails

--- SOLID.CleanArchitecture .NET.API/Model/CustomProblemDetails.cs
+++ SOLID.CleanArchitecture .NET.API/Model/CustomProblemDetails.cs
@@ -4,6 +4,7 @@ namespace SOLID.CleanArchitecture_.NET.API.Model
 {
     public class CustomProblemDetails : ProblemDetails
     {
+        //
         public IDictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
     }
 }
